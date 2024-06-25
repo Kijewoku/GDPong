3868699e28092dd9b7c9b645652be5bfb927be55
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2024-06-25 14:05:21 +0200

    feat: add bounce effect on the ball collision

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,14 +1,20 @@
 extends Area2D
 class_name Ball
 
-@export var move_speed: float = 1000.0
+@export var default_scale: Vector2 = Vector2(1, 1)
+@export var default_color: Color = Color(1, 1, 1, 1)
+@export var move_speed: float = 500.0
 @export var direction: Vector2 = Vector2.LEFT
 var bounce_sound: AudioStreamPlayer
+var animation_player: AnimationPlayer
 
 const STARTING_POINT = Vector2(640, 360)
 
 func _ready():
 	bounce_sound = $AudioStreamPlayer
+	animation_player = $AnimationPlayer
+	$Sprite2D.scale = default_scale
+	$Sprite2D.self_modulate = default_color
 
 func _physics_process(delta):
 	position += direction * move_speed * delta
@@ -20,3 +26,9 @@ func reset(direction: Vector2):
 func bounce(direction: Vector2):
 	self.direction = direction
 	bounce_sound.play()
+	animation_player.stop()
+	animation_player.play("bounce")
+
+func _on_Ball_body_entered(body):
+	if body.is_in_group("walls"):
+		bounce(direction.bounce(body.get_normal()))
